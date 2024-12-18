62ea3027731bc9ca2fcf56576bdc68c6773dfaa6
PU-MEriksson <malin.eriksson83@icloud.com>
2024-12-18 11:42:21 +0100

    Started working on the booking sequence

--- booking.php
+++ booking.php
@@ -1,21 +1,45 @@
 <?php
 
 declare(strict_types=1);
+require __DIR__ . '/includes/functions.php';
 
 if (isset($_POST['visitor_name'], $_POST['arrival_date'], $_POST['departure_date'], $_POST['room_id'], $_POST['transfer_code'])) {
 
-    //Variables used in the booking process, I need to sanitize these
-    $visitorName = $_POST['visitor_name'];
-    $arrivalDate = $_POST['arrival_date'];
-    $departureDate = $_POST['departure_date'];
-    $roomId = $_POST['room_id'];
-    $transferCode = $_POST['transfer_code'];
+    //Variables used in the booking process
+    $visitorName = sanitizeInput($_POST['visitor_name'] ?? '');
+    $arrivalDate = sanitizeInput($_POST['arrival_date'] ?? '');
+    $departureDate = sanitizeInput($_POST['departure_date'] ?? '');
+    $roomId = (int)($_POST['room_id'] ?? 0);
+    $transferCode = sanitizeInput($_POST['transfer_code'] ?? '');
 
-    echo "Name: $visitorName<br>";
-    echo "Arrival date: $arrivalDate <br>";
-    echo "Departure date: $departureDate <br>";
-    echo "Room id: $roomId <br>";
-    echo "Transfer code: $transferCode ";
+    //Validates the input so none of the forms are empty
+    if (validateBookingInput($visitorName, $arrivalDate, $departureDate, $roomId, $transferCode)) {
+
+        //The booking sequence
+        //Check if room is available, if room is available get the base price and total cost
+        if (isRoomAvailable($database, $roomId, $arrivalDate, $departureDate)) {
+            $basePrice = getRoomPrices($database, $roomId);
+            $totalCost = totalCost($basePrice, $arrivalDate, $departureDate);
+
+        } else {
+            echo "The room is not available on the chosen dates"
+        }
+
+        // Fetch room price 
+        // Calculate the total cost 
+        // Check availability 
+        // Validate the transfer code 
+        // Save booking 
+    }
+
+    //Test
+    // echo "Name: $visitorName<br>";
+    // echo "Arrival date: $arrivalDate <br>";
+    // echo "Departure date: $departureDate <br>";
+    // echo "Room id: $roomId <br>";
+    // echo "Transfer code: $transferCode ";
+
+
 }
 
 //Potential workflow for booking a room:
