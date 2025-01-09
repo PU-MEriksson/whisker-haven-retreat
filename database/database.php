3b627c32bcbc96debda8b746b0dc4d2b4d32f221
PU-MEriksson <malin.eriksson83@icloud.com>
2025-01-09 17:24:19 +0100

    Removed code that was commented out

--- database/database.php
+++ database/database.php
@@ -1,21 +1,14 @@
 <?php
 
 //Start a database connection
-// $database = new PDO('sqlite:' . __DIR__ . '/../database/hotel.db');
-// $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// $database = new PDO('sqlite:' . realpath(__DIR__ . '/../database/hotel.db'));
-// $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-
-// Kontrollera om databasfilen finns
+// Check if the database file exists
 if (!file_exists(__DIR__ . '/../database/hotel.db')) {
     die("Database file not found at: " . __DIR__ . '/../database/hotel.db');
 }
 
 try {
-    // Starta en databasanslutning
+    // Start a new connection
     $database = new PDO('sqlite:' . __DIR__ . '/../database/hotel.db');
     $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
